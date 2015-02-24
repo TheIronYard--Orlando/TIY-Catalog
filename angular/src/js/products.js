b1cbbc7d0b62289103a88e05b4a842f297b00571
Garrett Graham <ganderson7197@icloud.com>
2015-02-24 18:34:33 -0500

    sam got the basic grid layout with image h1 span for the price and a p tag for the description. instead of attempting the angular with the ng-repeat directive we decided to bootstrap the html in order to get a mvp to act then refactor with angular. I added some divs for the list view but need to lay it out appropriately possibly with a display block.

---
+++ angular/src/js/products.js
@@ -0,0 +1,19 @@
+(function(){
+    var app = angular.module("BetterThanEtsy", [ ]);
+    
+    var products = [{
+        title : "",
+        price : "",
+        description : "",
+        MainImage : [ {url_75x75 : "{{URL}}", url_170x135 : "{{URL}}"}]
+    }];
+    
+    app.controller('productController', function(){
+        this.product = item;
+    });
+    
+    
+    app.controller('navController', function(){
+        this.
+    });
+})();
