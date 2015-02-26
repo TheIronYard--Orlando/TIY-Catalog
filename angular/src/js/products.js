b9206dce3204e4098d9fd51f441df5d890caa415
Carolina Powers <carolinapoloni@gmail.com>
2015-02-25 23:26:51 -0500

    trying to populate date from json file

--- angular/src/js/products.js
+++ angular/src/js/products.js
@@ -1,25 +1,47 @@
 (function(){
     var app = angular.module("BetterThanEtsy", []);
     
-    var products = [{
-        title : "Widget0",
-        price : "$5.00",
-        description : "",
-        MainImage : [ 
-            {url_75x75 : "{{URL}}"},
-            { url_170x135 : "{{URL}}"}
-        ]
-    },
-        {title : "Widget2",
-        price : "$10.00",
-        description : "Anything",
-        MainImage : [ {url_75x75 : "http://lorempixel.com/160/160", url_170x135 : "{{URL}}"}]
-        }];
+    app.controller('StoreController', ['$http', function($http){
+//       this.items = products;
+         var self = this;
+         self.items = [];
+        
     
-    app.controller('StoreController', function(){
-       this.items = products;
-    });
-    
+        
+       $http.get('js/etsy.json').succes(function(data) {
+           self.items = data;
+        
+    })
+    } ])
+//    
+////    .controller('MainController', [ '$http', function($http){
+////      var self = this;
+// 
+////      $http.get('https://api.github.com/users/al-the-x')
+////        .success(function(data){
+////          self.user = data;
+////      })
+////    } ]) // END MainController
+//    
+//    var products = [{
+//        title : "Widget0",
+//        price : "$5.00",
+//        description : "",
+//        MainImage : [ 
+//            {url_75x75 : "{{URL}}"},
+//            { url_170x135 : "{{URL}}"}
+//        ]
+//    },
+//        {title : "Widget2",
+//        price : "$10.00",
+//        description : "Anything",
+//        MainImage : [ {url_75x75 : "http://lorempixel.com/160/160", url_170x135 : "{{URL}}"}]
+//        }];
+//    
+//    app.controller('StoreController', function(){
+//       this.items = products;
+//    });
+//    
     
    
 })();
