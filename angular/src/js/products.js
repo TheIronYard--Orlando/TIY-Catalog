20ad4a16a17df1b10683969a7fda08eccbd0b96d
Carolina Powers <carolinapoloni@gmail.com>
2015-02-26 00:00:42 -0500

    array of objects to represent data

--- angular/src/js/products.js
+++ angular/src/js/products.js
@@ -1,48 +1,104 @@
-(function(){
+(function () {
     var app = angular.module("BetterThanEtsy", []);
-    
-//    app.controller('StoreController', ['$http', function($http){
-////       this.items = products;
-//         var self = this;
-//         self.items = [];
-//        
-//    
-//        
-//       $http.get('js/etsy.json').succes(function(data) {
-//           self.items = data;
-//        
-//    })
-//    } ])
 
-    var products = [{
-        title : "Tittle0",
-        price : "$5.00",
-        description : "This is the most spectacular description of all time. Dont you think?",
-        MainImage : [ 
-            {
-                url_75x75 : "http://lorempixel.com/160/160"
-            },
-            { 
-                url_170x135 : "http://lorempixel.com/160/160"
-            }
-        ]
-    },
-        {title : "Tittle1",
-        price : "$10.00",
-        description : "This is the most spectacular description of all time. Dont you think?",
-        MainImage : [ 
-            {
-                url_75x75 : "http://lorempixel.com/160/160",
-            }, 
-            {
-                url_170x135 : "{{URL}}"
-            }]
-        }];
-    
-    app.controller('StoreController', function(){
-       this.items = products;
+    //    app.controller('StoreController', ['$http', function($http){
+    ////       this.items = products;
+    //         var self = this;
+    //         self.items = [];
+    //        
+    //    
+    //        
+    //       $http.get('js/etsy.json').succes(function(data) {
+    //           self.items = data;
+    //        
+    //    })
+    //    } ])
+
+
+
+    app.controller('StoreController', function () {
+        this.items = products;
     });
-    
-    
-   
+
+    var products = [
+        {
+            title: "Title1",
+            price: "$5.00",
+            description: "This is the most spectacular description of all time. Dont you think?",
+            MainImage: [
+                {
+                    url_75x75: "http://lorempixel.com/160/160"
+                },
+                {
+                    url_170x135: "http://lorempixel.com/160/160"
+                }
+            ]
+        },
+        {
+            title: "Title2",
+            price: "$10.00",
+            description: "This is the most spectacular description of all time. Dont you think?",
+            MainImage: [
+                {
+                    url_75x75: "http://lorempixel.com/160/160",
+                },
+                {
+                    url_170x135: "http://lorempixel.com/160/160"
+                }]
+        },
+        {
+            title: "Title3",
+            price: "$15.00",
+            description: "This is the most spectacular description of all time. Dont you think?",
+            MainImage: [
+                {
+                    url_75x75: "http://lorempixel.com/160/160",
+                },
+                {
+                    url_170x135: "http://lorempixel.com/160/160"
+                }]
+        },
+
+        {
+            title: "Title4",
+            price: "$20.00",
+            description: "This is the most spectacular description of all time. Dont you think?",
+            MainImage: [
+                {
+                    url_75x75: "http://lorempixel.com/160/160",
+                },
+                {
+                    url_170x135: "http://lorempixel.com/160/160"
+                }]
+        },
+
+        {
+            title: "Title5",
+            price: "$25.00",
+            description: "This is the most spectacular description of all time. Dont you think?",
+            MainImage: [
+                {
+                    url_75x75: "http://lorempixel.com/160/160",
+                },
+                {
+                    url_170x135: "http://lorempixel.com/160/160"
+                }]
+        },
+        {
+            title: "Title6",
+            price: "$30.00",
+            description: "This is the most spectacular description of all time. Dont you think?",
+            MainImage: [
+                {
+                    url_75x75: "http://lorempixel.com/160/160",
+                },
+                {
+                    url_170x135: "http://lorempixel.com/160/160"
+                }]
+        }
+
+        ];
+
+
+
 })();
