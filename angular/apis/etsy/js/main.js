903cc35680b7d15112522d36531d47c9602584f7
Taylor Foster <tagfoster90@gmail.com>
2015-02-25 22:25:05 -0500

    getting some data in the console

--- angular/apis/etsy/js/main.js
+++ angular/apis/etsy/js/main.js
@@ -1,1 +1,14 @@
 
+(function(){
+    angular.module('apiGymnastics', [ ])
+    .controller('MainController',[ '$http', function($http){
+    	var self = this;
+        $http.get('trending.json')
+        .success(function(data){
+    		for(i=0; i < data.results.length; i++){
+    			console.log(data.results[i].MainImage.url_fullxfull);
+    		}
+        });
+    } ]) //END MainController
+; //End tiy-catalog
+})();
