51610a49749e95fd756259210d1c47c1b20331eb
David Rogers <al.the.x@gmail.com>
2015-02-27 13:37:26 -0500

    Placeholder for Browser Sync goodies...

--- angular/gulpfile.js
+++ angular/gulpfile.js
@@ -1,8 +1,12 @@
  var gulp = require('gulp');
  var sass = require('gulp-sass');
- 
+
  gulp.task('sass', function() {
-	gulp.src('src/scss/*.scss')	 
+	gulp.src('src/scss/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('src/css'));
  });
+
+gulp.task('serve', function(){
+    // Do stuff in here with Browser Sync...
+});
