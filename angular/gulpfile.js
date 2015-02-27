c0cdff93d9febf2abfe0feb67f6b42081eb7ab10
Garrett Graham <ganderson7197@icloud.com>
2015-02-27 16:51:06 -0500

    ran gulp sass and it created the products.css file. now trying to figure out how to get it to render with browser-sync. we now have to replace the panel and try to use ng class to consolidate the code within the two tab panels. we need to apply classes to show  either the grid view or the list view. only one piece of html code for both the grid and list layout instead of coding for two sections like we did the first time around

--- angular/gulpfile.js
+++ angular/gulpfile.js
@@ -1,8 +1,29 @@
  var gulp = require('gulp');
  var sass = require('gulp-sass');
- 
- gulp.task('sass', function() {
-	gulp.src('src/scss/*.scss')	 
-		.pipe(sass())
-		.pipe(gulp.dest('src/css'));
+ var browser - sync = require('browser-sync');
+
+ gulp.task('sass', function () {
+     return gulp.src('src/scss/*.scss')
+         .pipe(sass())
+         .pipe(gulp.dest('src/css'));
  });
+
+ gulp.task('watch', function () {
+     gulp.watch('scss/*.scss', ['sass']);
+ });
+
+ gulp.task('default', ['sass', 'watch']);
+
+
+
+
+ gulp.task('browser-sync', function () {
+             browser - sync({
+                         server: {
+                             baseDir: "./" /*is this supposed to be like ./css or is the "./" notation the one to use*/
+                         }
+                         gulp.task('browser-sync', function () {
+                             browserSync({
+                                 proxy: "yourlocal.dev"
+                             });
+                         });
