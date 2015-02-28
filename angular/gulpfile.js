48c8a75c27acb71b314939bfba4ade51508ab5be
Garrett Graham <ganderson7197@icloud.com>
2015-02-28 14:17:55 -0500

    added the working code to run gulp sass but working on how to get it with browser sync. i left in the codebut commented it out

--- angular/gulpfile.js
+++ angular/gulpfile.js
@@ -1,23 +1,39 @@
+//var gulp = require('gulp');
+////var browser-sync = require('browser-sync');
+//var sass = require('gulp-sass');
+////var reload = browsersync.reload;
+//
+//
+//gulp.task('serve', ['sass'], function () {
+//    browser - sync({
+//        server: "./app"
+//    });
+//
+//    gulp.watch('scss/*.scss', ['sass']);
+//    gulp.watch('src/products.html').on('change', reload);
+//});
+//
+//gulp.task('sass', function () {
+//    return gulp.src('src/scss/*.scss')
+//        .pipe(sass())
+//        .pipe(gulp.dest('src/css'));
+//    .pipe(reload({
+//        stream: true
+//    }));
+//});
+
+
+// !!The below code works for $ gulp sass!!
+
 var gulp = require('gulp');
-var browser - sync = require('browser-sync');
 var sass = require('gulp-sass');
-var reload = browsersync.reload;
 
+ gulp.task('sass', function () {
+     return gulp.src('src/scss/*.scss')
+         .pipe(sass())
+         .pipe(gulp.dest('src/css'));
+ });
 
-gulp.task('serve', ['sass'], function () {
-    browser - sync({
-        server: "./app"
-    });
-
-    gulp.watch('scss/*.scss', ['sass']);
-    gulp.watch('src/products.html').on('change', reload);
-});
-
-gulp.task('sass', function () {
-    return gulp.src('src/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('src/css'));
-    .pipe(reload({
-        stream: true
-    }));
-});
+ gulp.task('watch', function () {
+     gulp.watch('scss/*.scss', ['sass']);
+ });
