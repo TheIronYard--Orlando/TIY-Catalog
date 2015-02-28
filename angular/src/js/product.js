2b5eeb5da6b1856eab05dea358a0ea231d0f1fc6
Josuel C Garcia <garciajosuel@yahoo.com>
2015-02-28 14:47:25 -0500

    ng repeat is cool

---
+++ angular/src/js/product.js
@@ -0,0 +1,15 @@
+(function () {
+    angular.module('elproduct', [])
+        .controller('ProductController', function () {
+            this.prodArray = [1, 2, 3, 4, 5, 6];
+        })
+
+
+
+
+
+
+
+
+    ; // closes module  
+})();
