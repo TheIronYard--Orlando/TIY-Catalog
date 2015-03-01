c3c4ab72aa53bccae5e07bdc417fadd5be9f16e8
Carolina Powers <carolinapoloni@gmail.com>
2015-03-01 10:28:14 -0500

    adding files that i probably do not need

---
+++ angular/gulpfile_REMOTE_28064.js
@@ -0,0 +1,41 @@
+//var gulp = require('gulp');
+////var browser-sync = require('browser-sync');
+//var sass = require('gulp-sass');
+////var reload = browsersync.reload;
+//
+//
+//gulp.task('serve', ['sass'], function () {
+//    browser - sync({
+//        server: "./app"
+//    });
+//
+//    gulp.watch('scss/*.scss', ['sass']);
+//    gulp.watch('src/products.html').on('change', reload);
+//});
+//
+//gulp.task('sass', function () {
+//    return gulp.src('src/scss/*.scss')
+//        .pipe(sass())
+//        .pipe(gulp.dest('src/css'));
+//    .pipe(reload({
+//        stream: true
+//    }));
+//});
+//
+//gulp.task('default', ['serve']);
+
+
+// !!The below code works for $ gulp sass!!
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+ gulp.task('sass', function () {
+     return gulp.src('src/scss/*.scss')
+         .pipe(sass())
+         .pipe(gulp.dest('src/css'));
+ });
+
+ gulp.task('watch', function () {
+     gulp.watch('scss/*.scss', ['sass']);
+ });
