9a5bb192782fc54ea490ab302aa02f596fa2799b
Garrett Graham <ganderson7197@icloud.com>
2015-02-28 23:18:34 -0500

    got the browser sync to work thanks to the help of carolina. updated the files and hoping to update my fork

--- angular/gulpfile.js
+++ angular/gulpfile.js
@@ -1,9 +1,119 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+
+gulp.task('sass', function () {
+    gulp.src('src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('server', function () {
+    browserSync({
+        server: {
+            baseDir: "./src",
+            index: "products.html",
+            routes: {
+                "/bower_components": "bower_components"
+            }
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//backup code reference below
+
+
 //var gulp = require('gulp');
-////var browser-sync = require('browser-sync');
 //var sass = require('gulp-sass');
-////var reload = browsersync.reload;
+//var browserSync = require('browser-sync');
 //
 //
+//gulp.task('sass', function () {
+//    return gulp.src('src/scss/*.scss')
+//        .pipe(sass({includePaths: ['scss']}))
+//        .pipe(gulp.dest('css'));
+//});
+//
+//gulp.task('browser-sync', function () {
+//    browserSync.init(["css/products.css", "js/products.js"], {
+//        server: {
+//            baseDir: "./"
+//        }
+//    });
+//});
+//
+////
+////gulp.task('server', function () {
+////    browserSync({
+////        server: {
+////            baseDir: "./src",
+////            index: "products.html",
+////            routes: {
+////                "/bower_components": "bower_components"
+////            }
+////        }
+////    });
+////});
+//
+//
+//gulp.task('default', ['sass', 'browser-sync'], function () {
+//    gulp.watch('scss/products.scss', ['sass']);
+//});
+
+
+
+
+
+//var gulp = require('gulp');
+//var sass = require('gulp-sass');
+//var browser-sync = require('browser-sync');
+//
+//
+//gulp.task('sass', function () {
+//    return gulp.src('src/scss/*.scss')
+//        .pipe(sass())
+//        .pipe(gulp.dest('src/css'));
+//});
+
+//gulp.task('watch', function () {
+//    gulp.watch('scss/*.scss', ['sass']);
+//});
+//
+//gulp.task('browser-sync', function() {
+//    browserSync.init(["css/products.css", "js/products.js"], {
+//        server: {
+//            baseDir: "./"
+//        }
+//    });
+//});
+//
+//gulp.task('default', ['sass', 'browser-sync'], function() {
+//    gulp.watch('scss/*.scss', ['sass']);
+//});
+//
+
+//var reload = browsersync.reload;
+
 //gulp.task('serve', ['sass'], function () {
 //    browser - sync({
 //        server: "./app"
@@ -23,19 +133,3 @@
 //});
 //
 //gulp.task('default', ['serve']);
-
-
-// !!The below code works for $ gulp sass!!
-
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
- gulp.task('sass', function () {
-     return gulp.src('src/scss/*.scss')
-         .pipe(sass())
-         .pipe(gulp.dest('src/css'));
- });
-
- gulp.task('watch', function () {
-     gulp.watch('scss/*.scss', ['sass']);
- });
