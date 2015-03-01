45bde10a25b9698311ca59375ce5773212df0b21
Manatazach Lawya <manatazach144@Manatazachs-MacBook-Pro.local>
2015-02-28 23:32:20 -0500

    add images to listing and product page

--- rails/etsy_product_catalog/app/models/listing.rb
+++ rails/etsy_product_catalog/app/models/listing.rb
@@ -1,6 +1,8 @@
 class Listing < ActiveRecord::Base
  has_many :images, foreign_key: :etsy_listing_id
 
-
+ def main_image
+  Image.where(etsy_listing_id: etsy_listing_id).first.url_fullxfull
+ end
 
 end
