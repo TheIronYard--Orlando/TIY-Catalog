f91451b11717ebf1497d8d33f2f115f0311584a7
Jon Rutishauser <jrutishauser@gmail.com>
2015-03-01 15:47:15 -0500

    sassified some of the content, cant figure out how to condense the bootstrap files

--- angular/gulpfile.js
+++ angular/gulpfile.js
@@ -1,8 +1,28 @@
  var gulp = require('gulp');
  var sass = require('gulp-sass');
- 
+ var notify = require('gulp-notify');
+ var bower = require('gulp-bower');
+ var config = {
+ 	sassPath: './src/scss',
+ 	bowerDir: './bower_components'
+ };
+ gulp.task('bower', function(){
+ 	return bower()
+ 	.pipe(gulp.dest(config.bowerDir));
+ });
+
+
  gulp.task('sass', function() {
 	gulp.src('src/scss/*.scss')	 
-		.pipe(sass())
-		.pipe(gulp.dest('src/css'));
+		.pipe(sass({
+			style: 'compressed',
+			loadPath: [
+			'./src/scss',
+			config.bowerDir + '/bootstrap'
+			]
+		})
+			 .on('error', notify.onError(function (error){
+			 	return 'Error: ' + error.message;
+			 })))
+		.pipe(gulp.dest('./src/css'));
  });
