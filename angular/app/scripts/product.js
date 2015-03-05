3e058f463c647a87014c432f456caa9ab6ee2a5e
Jon Rutishauser <jrutishauser@gmail.com>
2015-03-05 12:00:14 -0500

    product cycles through images in an array in the js file

--- angular/app/scripts/product.js
+++ angular/app/scripts/product.js
@@ -4,11 +4,18 @@
             this.prodArray = [1, 2, 3, 4, 5, 6];
 			this.moused = function(){
 				console.log('mouseover');
-				alert('I hate you');		
 			};
-       		this.moarpics = function(){
-				console.log('clicked!');	
-			
+			this.pics = [
+			'http://www.q-tees.co.uk/image/cache/data/t-shirts2/html%20green-542x700.jpg',
+			'http://www.gekius.com/image/cache/data/products/sourcecontrol/github/code%20sucks-600x800.jpg'		
+			];
+			this.imgnum = 0;
+       		this.morepics = function(){
+				console.log('clicked!');
+				this.imgnum++;
+				if (this.imgnum === this.pics.length){
+					this.imgnum = 0;	
+				}	
 			}; 
 		})
 
