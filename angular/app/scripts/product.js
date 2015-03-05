5dcb7a0518cba7b0bf5402d48f7c49d8f2adce8c
Jon Rutishauser <jrutishauser@gmail.com>
2015-03-05 10:41:19 -0500

    mouseover event does not execture function, ng-click however does work... interesting.

--- angular/app/scripts/product.js
+++ angular/app/scripts/product.js
@@ -2,7 +2,15 @@
     angular.module('elproduct', [])
         .controller('ProductController', function () {
             this.prodArray = [1, 2, 3, 4, 5, 6];
-        })
+			this.moused = function(){
+				console.log('mouseover');
+				alert('I hate you');		
+			};
+       		this.moarpics = function(){
+				console.log('clicked!');	
+			
+			}; 
+		})
 
 
 
